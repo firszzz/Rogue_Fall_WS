9ad4d8e9f89a2b0514fde01518f7e2b9f29466b7
Andrey <n1an27f@gmail.com>
2022-05-14 23:27:56 +1000

    Add all

---
+++ script.js
@@ -0,0 +1,56 @@
+let music = new Audio("static/bg-sound.mp3");
+let text_music_button = document.getElementById("m_b");
+let flag = false;
+
+function cli() {
+    console.log("AA")
+    if (flag === false) {
+        music.play();
+        music.volume = 0.05;
+        music.loop = true;
+        flag = true;
+        text_music_button.innerText = "♫ Выключить музыку";
+    }
+    else if (flag === true) {
+        music.pause();
+        flag = false;
+        text_music_button.innerText = "♫ Включить музыку"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
